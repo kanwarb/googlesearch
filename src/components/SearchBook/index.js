32947c2a9cdb402f5ec2b2f0d659c6b7fa20f3af
KB Total Recall <kanwar@gmail.com>
2019-04-28 15:25:29 -0700

    Add logic to button to view book on google books site

--- src/components/SearchBook/index.js
+++ src/components/SearchBook/index.js
@@ -4,23 +4,25 @@ import "./style.css";
 // Using the datalist element we can create autofill suggestions based on the props.breeds array
 function SearchBook(props) {
   return (
+    <div>
     <form className="search">
       <div className="form-group">
-        <label htmlFor="books">Book Search:</label>
+        <label htmlFor="books" className="align:center">Book Search:</label>
         <input
           value={props.search}
           onChange={props.handleInputChange}
           name="book"
           type="text"
-          className="form-control"
+          className="form-control input float-left"
           placeholder="Type in book to search"
           id="book"
         />
-        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success button float-left">
           Search
         </button>
       </div>
     </form>
+    </div>
   );
 }
 
